988fdaa927b2f08fab2ede46f4b5a8372cb48323
Julieta Laplace <juli.laplace@gmail.com>
2022-08-22 22:45:47 -0300

    ejercicios primera clase Programacion II

---
+++ EjercitacionClase2D-LaplaceJulieta/Ejercicio03/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Ejercicio03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            bool numeroIngresado;
+
+            Console.WriteLine("Ingrese un numero:");
+            numeroIngresado = int.TryParse(Console.ReadLine(), out numero);
+            if (numeroIngresado)
+            {
+
+            }
+        }
+    }
+}
